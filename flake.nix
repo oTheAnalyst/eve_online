e7a0c801d8035f88112013f0dcbb80398497478d
VtheRtech <michaelvcumbo21@proton.me>
2026-02-18 08:15:11 -0500

    fixed nix utlies

--- flake.nix
+++ flake.nix
@@ -1,20 +1,24 @@
 {
-  description = "Dev shell using external derivation";
-
+  flake-utils,
+  nixpkgs,
+  ...
+}: {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        # replace ./derivation.nix with the path (relative to this file) to your derivation
-        eveauth = pkgs.python3Packages.callPackage ./shell.nix {
-          lib = pkgs.lib;
-        };
-      in {
-        packages.default = pkgs.python3.withPackages(_: [ eveauth ]);
-      });
+  outputs = {
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      # replace ./derivation.nix with the path (relative to this file) to your derivation
+      eveauth = pkgs.python3Packages.callPackage ./shell.nix {
+        inherit (pkgs) lib;
+      };
+    in {
+      packages.default = pkgs.python3.withPackages (_: [eveauth]);
+    });
 }
